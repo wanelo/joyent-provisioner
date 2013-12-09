5dc1c3738e6e7c6b55cb2738f7fded6986f51a38
Konstantin Gredeskoul <kig@GrooverPooper.local>
2013-12-08 17:43:59 -0800

    Stripping Total from server list output

--- lib/provisioner/host_ip.rb
+++ lib/provisioner/host_ip.rb
@@ -15,13 +15,14 @@ module Provisioner
         # example line
         # 4330e738-2bca-ee10-f9a6-e9a1b3fcfdec  vpn001.prod running  virtualmachine  sdc:jpc:ubuntu-12.04:2.4.  g3-highcpu-1.75-kvm  9.12.42.172,10.100.114.217  1.75 GB  75 GB
         zone, hostname, status, type, image, flavor, ips = server.split(/\s+/).compact
+        next if ips.nil?
         @ips[hostname] = (ips.split(',') || []).first
       end
       @ips
     end
 
     def self.server_list
-      @server_list ||= `knife joyent server list | tail +2`.split("\n")
+      @server_list ||= `knife joyent server list | tail +2 | grep -v "Total  "`.split("\n")
     end
   end
 end
