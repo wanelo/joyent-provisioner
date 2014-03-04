585c26ad94a258cdf0277f8413a226af9320aafc
bixu <bixv@icloud.com>
2014-03-04 19:38:51 +0100

    corrected order of ops

--- lib/provisioner/command/provision.rb
+++ lib/provisioner/command/provision.rb
@@ -21,9 +21,9 @@ module Provisioner
 
         log_path = "#{log_dir}/#{host_name}_provision.log"
         command << "--run-list #{run_list}" if run_list
+        command << "--ssh-user #{ssh_user}" if ssh_user
         command << "--tags '#{encoded_tags}'" if tags
         command << "2>&1 > #{log_path} &"
-        command << "--ssh-user '#{ssh_user}'" if ssh_user
 
         command.join(' ')
       end
